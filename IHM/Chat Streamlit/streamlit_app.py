2766c2594f7933c44468acc3772796409ad91f3f
Alban Kerloc'h <alban.kerloch@gmail.com>
2024-12-27 14:39:21 +0000

    Chat avec Streamlit V1

--- IHM/Chat Streamlit/streamlit_app.py
+++ IHM/Chat Streamlit/streamlit_app.py
@@ -10,11 +10,21 @@ API_KEY = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
+st.session_state.messages = []
+st.session_state.gemini_history = []
+st.session_state.model = model
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[])
+st.session_state.chat_title = 'ChatSession-1'
+st.session_state.chat_id = 1
 
 if 'uploaded_file' not in st.session_state:
     st.session_state['uploaded_file'] = None
 if 'upload_mode' not in st.session_state:
     st.session_state['upload_mode'] = False
+
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[])
 
 def call_gemini_text(prompt):
     response = model.generate_content(prompt)
@@ -25,6 +35,26 @@ def call_gemini_image(prompt, image):
     response = model.generate_content([prompt, image[0], prompt])
     rep = response.candidates[0].content.parts[0].text
     return rep
+
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        image_parts = [{"mime_type": uploaded_file.type, "data": bytes_data}]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+def map_role(role):
+    if role == "model":
+        return "assistant"
+    else:
+        return role
+
+def fetch_gemini_response(user_query):
+    # Use the session's model to generate a response
+    response = st.session_state.chat_session.model.generate_content(user_query)
+    print(f"Gemini's Response: {response}")
+    return response.parts[0].text
 
 st.title("Assistance à la Maintenance")
 
@@ -57,14 +87,6 @@ user_quest = st.text_input("Saisir votre question :")
 
 btn = st.button("Demander")
 
-def input_image_setup(uploaded_file):
-    if uploaded_file is not None:
-        bytes_data = uploaded_file.getvalue()
-        image_parts = [{"mime_type": uploaded_file.type, "data": bytes_data}]
-        return image_parts
-    else:
-        raise FileNotFoundError("No file uploaded")
-
 if btn and user_quest:
     if st.session_state['uploaded_file'] is not None:
         image_data = input_image_setup(st.session_state['uploaded_file'])
@@ -73,5 +95,16 @@ if btn and user_quest:
         result = call_gemini_text(user_quest)
     st.write(result)
 
+for msg in st.session_state.chat_session.history:
+    with st.chat_message(map_role(msg["role"])):
+        st.markdown(msg["content"])
 
-    
+user_input = st.chat_input("Demander à Gemini ...")
+
+if user_input:
+    st.chat_message("user").markdown(user_input)
+    gemini_response = fetch_gemini_response(user_input)
+    with st.chat_message("assistant"):
+        st.markdown(gemini_response)
+    st.session_state.chat_session.history.append({"role": "user", "content": user_input})
+    st.session_state.chat_session.history.append({"role": "model", "content": gemini_response})
