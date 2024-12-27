8bcb760ea988cba2d23a10e9836bdd3febacb021
Alban Kerloc'h <alban.kerloch@gmail.com>
2024-12-27 15:49:57 +0000

    Ajout historique

--- IHM/Chat Streamlit/streamlit_app.py
+++ IHM/Chat Streamlit/streamlit_app.py
@@ -8,32 +8,35 @@ import os
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
 
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
-st.session_state.messages = []
-st.session_state.gemini_history = []
-st.session_state.model = model
-if "chat_session" not in st.session_state:
-    st.session_state.chat_session = model.start_chat(history=[])
-st.session_state.chat_title = 'ChatSession-1'
-st.session_state.chat_id = 1
+MODEL_ROLE = 'ai'
+AI_AVATAR_ICON = '✨'
+
+
+# Initialisation de l'état de session pour l'historique
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+if 'gemini_history' not in st.session_state:
+    st.session_state.gemini_history = []
+
+# Configuration du modèle et du chat
+st.session_state.model = genai.GenerativeModel('gemini-1.5-flash')
+st.session_state.chat_session = st.session_state.model.start_chat(
+    history=st.session_state.gemini_history,
+)
 
 if 'uploaded_file' not in st.session_state:
     st.session_state['uploaded_file'] = None
 if 'upload_mode' not in st.session_state:
     st.session_state['upload_mode'] = False
 
-if "chat_session" not in st.session_state:
-    st.session_state.chat_session = model.start_chat(history=[])
-
 def call_gemini_text(prompt):
-    response = model.generate_content(prompt)
-    rep = response.candidates[0].content.parts[0].text
+    response = st.session_state.chat_session.send_message(prompt)
+    rep = response.parts[0].text
     return rep
 
 def call_gemini_image(prompt, image):
-    response = model.generate_content([prompt, image[0], prompt])
-    rep = response.candidates[0].content.parts[0].text
+    response = st.session_state.chat_session.send_message([prompt, image[0], prompt])
+    rep = response.parts[0].text
     return rep
 
 def input_image_setup(uploaded_file):
@@ -43,18 +46,6 @@ def input_image_setup(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
-
-def map_role(role):
-    if role == "model":
-        return "assistant"
-    else:
-        return role
-
-def fetch_gemini_response(user_query):
-    # Use the session's model to generate a response
-    response = st.session_state.chat_session.model.generate_content(user_query)
-    print(f"Gemini's Response: {response}")
-    return response.parts[0].text
 
 st.title("Assistance à la Maintenance")
 
@@ -83,20 +74,28 @@ if st.session_state['uploaded_file'] is not None:
         img = Image.open(st.session_state['uploaded_file'])
         st.image(img, caption='Image à envoyer', width=100)
 
-for msg in st.session_state.chat_session.history:
-    with st.chat_message(map_role(msg["role"])):
-        st.markdown(msg["content"])
+for message in st.session_state.messages:
+    with st.chat_message(
+        name=message['role'],
+        avatar=message.get('avatar'),
+    ):
+        st.markdown(message['content'])
 
 user_input = st.chat_input("Demander à Gemini ...")
 
+
+user_input = st.chat_input('Your message here...')
+
 if user_input:
-    st.chat_message("user").markdown(user_input)
+    with st.chat_message('user'):
+        st.markdown(user_input)
+    st.session_state.messages.append(dict(role='user',content=user_input))
     if st.session_state['uploaded_file'] is not None:
         image_data = input_image_setup(st.session_state['uploaded_file'])
         result = call_gemini_image(user_input, image_data)
     else:
         result = call_gemini_text(user_input)
-    with st.chat_message("assistant"):
+    with st.chat_message(name=MODEL_ROLE, avatar=AI_AVATAR_ICON):
         st.markdown(result)
-    st.session_state.chat_session.history.append({"role": "user", "content": user_input})
-    st.session_state.chat_session.history.append({"role": "model", "content": result})
+    st.session_state.messages.append(dict(role=MODEL_ROLE, content=result, avatar=AI_AVATAR_ICON))
+    st.session_state.gemini_history = st.session_state.chat_session.history
